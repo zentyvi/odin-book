50443b30d439d2404e737cf86f67f3a0609de987
goodmorningvo3@gmail.com <goodmorningvo3@gmail.com>
2026-09-15 21:02:08 +0300

    Add 'VITE' prefix to all env variables

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,9 +1,9 @@
 const api_version = import.meta?.env?.VITE_API_VERSION || "v1";
-const port = import.meta.env.PORT || 3000;
+const port = import.meta?.env?.VITE_PORT || 3000;
 
 const dev_server = `http://localhost:${port}`;
-const production_server = import.meta.env.VITE_SERVER_URL;
-const isDev = Boolean(import.meta.env.DEV);
+const production_server = import.meta?.env?.VITE_SERVER_URL;
+const isDev = Boolean(import.meta?.env?.VITE_DEV || 0);
 
 const api_url = isDev
   ? `${dev_server}/api/${api_version}`
