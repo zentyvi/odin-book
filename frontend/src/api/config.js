d37c50f154fe5b41433e8d98990d3891df354814
goodmorningvo3@gmail.com <goodmorningvo3@gmail.com>
2026-09-15 21:24:50 +0300

    Fix env variable

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -3,7 +3,7 @@ const port = import.meta?.env?.VITE_PORT || 3000;
 
 const dev_server = `http://localhost:${port}`;
 const production_server = import.meta?.env?.VITE_SERVER_URL;
-const isDev = Boolean(import.meta?.env?.VITE_DEV || 0);
+const isDev = Boolean(Number(import.meta.env["VITE_DEV"]));
 
 const api_url = isDev
   ? `${dev_server}/api/${api_version}`
